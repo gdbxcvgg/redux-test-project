8e321bd0c4156bce94701935d64cedcfd498bf8a
Dominik Koziatek <harrty.dev@gmail.com>
2026-09-03 21:41:29 +0200

    feat(02_lesson): add users feature

---
+++ 02_lesson/src/features/users/usersSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, nanoid, type PayloadAction } from '@reduxjs/toolkit'
+import type { RootStore } from '../../app/store'
+
+type User = {
+	id: string
+	username: string
+	displayName: string
+}
+
+const initialState: User[] = [
+	{
+		id: '1',
+		username: 'torvalds',
+		displayName: 'Linus Torvalds',
+	},
+	{
+		id: '2',
+		username: 'tdavis',
+		displayName: 'Terry Davis',
+	},
+]
+
+const usersSlice = createSlice({
+	name: 'users',
+	initialState,
+	reducers: {
+		addUser: {
+			reducer: (state: User[], action: PayloadAction<User>) => {
+				state.push(action.payload)
+			},
+			prepare: (username: string, displayName: string) => ({
+				payload: {
+					id: nanoid(),
+					username,
+					displayName,
+				},
+			}),
+		},
+	},
+})
+
+export const selectAllUsers = (state: RootStore) => state.users
+
+export const { addUser } = usersSlice.actions
+
+export default usersSlice.reducer
